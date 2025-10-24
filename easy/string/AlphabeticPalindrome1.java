ef87d717d744c16a27120ac0a7fd19937f867596
Prashant Sharma <ps.arraha@gmail.com>
2025-10-24 19:37:01 +0200

    Create AlphabeticPalindrome1.java

---
+++ easy/string/AlphabeticPalindrome1.java
@@ -0,0 +1,31 @@
+// https://www.hackerrank.com/contests/software-engineer-prep-kit/challenges/check-palindrome-filter-non-letters/problem?
+
+public class App {
+
+    public static boolean isAlphabeticPalindrome(String code) {
+        if (code == null || code.isEmpty()) {
+            return false;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : code.toCharArray()) {
+            if (Character.isAlphabetic(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+
+        String filtered = sb.toString();
+        int left = 0;
+        int right = filtered.length() - 1;
+
+        while (left < right) {
+            if (filtered.charAt(left) != filtered.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
